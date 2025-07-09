ed3a653ff846a3d606cf749e428df19613bdbf04
adeebbayat <adeebbayat@yahoo.com>
2025-07-08 19:21:58 -0700

    appa

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-print('Hello World')
+class MyCat:
+    def __init__(self,name):
+        self.name = name
+    
+    def show_name(self):
+        print(f"My cat's name is {self.name}")
+    
+x = MyCat("Appa")
+
+print(x)
+print(x.name)
